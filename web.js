fb361f92221686caa5765519d1b16a2731d0f9fa
hikaMaeng <hika00@gmail.com>
2013-12-02 17:56:04 +0900

    aaa

--- web.js
+++ web.js
@@ -1,2 +1,10 @@
+/*
 var bs = require('./bs/bsnode.js');
 bs.ROUTER.route( 8080, '../node', '_.js' );
+*/
+
+var server = require('http').createServer();
+server.on('request', function router( $rq, $rp ){
+	$rp.writeHead( 200, {'Content-Type':'text/html'} );
+	$rp.end( "aaaaaaaaaaa" );
+} );
