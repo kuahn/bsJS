43f79210e2e029aa5aaab21a0e98dba5d4ab7c98
hikaMaeng <hika00@gmail.com>
2013-12-05 00:49:50 +0900

    w

--- web.js
+++ web.js
@@ -1,2 +1,10 @@
+/*
 var bs = require('./bs/bsnode');
 bs.ROUTER.route( 8001, '../node', '_.js' );
+*/
+var http = require("http");
+http.createServer(function(request, response) {
+response.writeHead(200, {"Content-Type": "text/html"});
+response.write("Hello, World~!!");
+response.end();
+}).listen(8001);
